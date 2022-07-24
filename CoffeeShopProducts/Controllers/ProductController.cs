1e52e55a646b6967a135b7f1a478970eb574f9c2
AliAlHashemi14 <alialhashemi14@gmail.com>
2022-07-24 02:43:31 -0400

    First commit

---
+++ CoffeeShopProducts/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using CoffeeShopProducts.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopProducts.Controllers
+{
+    public class ProductController : Controller
+    {
+        private CoffeeShopDBContext context = new CoffeeShopDBContext();
+
+        public IActionResult Index()
+        {
+            List<Product> result = context.Products.ToList();
+            return View(result);
+        }
+
+        public IActionResult Detail(int postId)
+        {
+            Product p = context.Products.FirstOrDefault(p => p.Id == postId);
+            return View(p);
+        }
+    }
+}
